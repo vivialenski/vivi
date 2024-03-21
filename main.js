a731a37d1f4a8b96af2518dfeed330da600f95bd
vivialenski <164400394+vivialenski@users.noreply.github.com>
2024-03-21 15:35:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+}
+var frutas = ["Maçã", "Banana", "Laranja", "Pera"];
+for (var i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
+//Código omitido
+
+const contadores = document.querySelectorAll(".contador");
+
+//Código omitido
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
